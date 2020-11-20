09b80993caa7a13ed12d198e8e428e0e8ee6f42b
Sam Thurman <sam.bbmgmt@gmail.com>
2020-11-20 14:19:38 -0800

    built-in array method notes

---
+++ built-in-functions.js
@@ -0,0 +1,33 @@
+/*
+ARRAY.PROTOTYPE METHODS
+*/
+
+/*Array.prototype.reduce(func) --- non-mutative*/
+// A function that accepts up to four arguments. Calls the specified callback function 
+// once for each element in an array. The return value of the callback function is the 
+// accumulated result, and is provided as an argument in the next call to the callback 
+// function.
+
+let arr = [1, 2, 3, 4, 5];
+console.log(arr.reduce((sum, val) => sum + val)) // should be 10 (1+2+3+4+5)
+/*
+OBJECT.PROTOTYPE METHODS
+*/
+
+
+/*
+REGEX METHODS
+*/
+
+
+/*
+STRING.PROTOTYPE METHODS
+*/
+
+
+/*
+NUMBER.PROTOTYPE METHODS
+*/
+
+
+
