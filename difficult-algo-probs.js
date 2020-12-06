eb6d842b300b64197d2c1f5163e45322abe80b97
Sam Thurman <sam.bbmgmt@gmail.com>
2020-12-06 14:55:34 -0800

    orbital period conversion func

--- difficult-algo-probs.js
+++ difficult-algo-probs.js
@@ -109,3 +109,32 @@ function addTogether(first, second) {
 
 addTogether(2, 3) // should return 5.
 addTogether(23, 30) // should return 53.
+
+// Return a new array that transforms the elements' average altitude into their orbital periods 
+// (in seconds). The array will contain objects in the format {name: 'name', avgAlt: avgAlt}.
+// You can read about orbital periods on Wikipedia.
+// The values should be rounded to the nearest whole number. The body being orbited is Earth.
+// The radius of the earth is 6367.4447 kilometers, and the GM value of earth is 398600.4418 km3s-2.
+
+function orbitalPeriod(arr) {
+    var GM = 398600.4418;
+    var earthRadius = 6367.4447;
+    var master = [];
+  
+  
+    // get val of avgAlt
+    // convert avgAlt to orbital periods
+      // rounded to nearest whole
+      // body being orbited is earth
+    // return new arr
+    for (let i in arr){
+      let alt = arr[i].avgAlt;
+      let a = (earthRadius+alt);
+      let t = Math.round((2*Math.PI)*Math.sqrt((a**3)/GM)); 
+      master.push({name: arr[i].name, orbitalPeriod: t});
+    }
+    return master
+  }
+// should return [{name: "sputnik", orbitalPeriod: 86400}]
+orbitalPeriod([{name : "sputnik", avgAlt : 35873.5553}]);
+  
