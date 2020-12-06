83aa5de564150594e703011e75600b6b0fce2dec
Sam Thurman <sam.bbmgmt@gmail.com>
2020-12-06 15:09:26 -0800

    palindrome checker algo, could improve on memory use

--- difficult-algo-probs.js
+++ difficult-algo-probs.js
@@ -137,4 +137,32 @@ function orbitalPeriod(arr) {
   }
 // should return [{name: "sputnik", orbitalPeriod: 86400}]
 orbitalPeriod([{name : "sputnik", avgAlt : 35873.5553}]);
+
+
+// Return true if the given string is a palindrome. Otherwise, return false.
+// A palindrome is a word or sentence that's spelled the same way both forward and backward, ignoring 
+// punctuation, case, and spacing.
+// Note
+// You'll need to remove all non-alphanumeric characters (punctuation, spaces and symbols) and turn 
+// everything into the same case (lower or upper case) in order to check for palindromes.
+// We'll pass strings with varying formats, such as "racecar", "RaceCar", and "race CAR" among others.
+// We'll also pass strings with special symbols, such as "2A3*3a2", "2A3 3a2", and "2_A3*3#A2".
+
+function palindrome(str) {
+    let reg = /[\W_\s]/g;
+    let matches = str.match(reg);
+    str = str.toLowerCase().replace(reg, '');
+  
+    console.log(str.split('').reverse().join(''))
+    console.log(str.split('').reverse().join('')===str)
+    
+    return str.split('').reverse().join('')===str;
+  
+  }
+  
+  
+// should return true
+palindrome("eye"); 
+// should return true
+palindrome("A man, a plan, a canal. Panama") 
   
