38294966b37b3a31ec71ed4ff0ac749643206fba
Sam Thurman <sam.bbmgmt@gmail.com>
2020-12-07 10:55:41 -0800

    telephone regex and caesars cypher (ROT13)

--- difficult-algo-probs.js
+++ difficult-algo-probs.js
@@ -2,13 +2,11 @@
 an array of all objects that have matching name and value pairs (second argument). 
 Each name and value pair of the source object has to be present in the object from 
 the collection if it is to be included in the returned array.
-
 For example, if the first argument is [{ first: "Romeo", last: "Montague" }, 
 { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], 
 and the second argument is { last: "Capulet" }, then you must return the third object 
 from the array (the first argument), because it contains the name and its value, that 
 was passed on as the second argument. */
-
 function whatIsInAName(collection, source) {
     var arr = [];
     var sourceKeys = Object.keys(source);
@@ -39,7 +37,7 @@ function whatIsInAName(collection, source) {
 console.log(whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" }));
 
 
-/* Convert a string to spinal case. Spinal case is all-lowercase-words-joined-by-dashes. */
+/* Convert a string to spinal case. Spinal case is all-lowercase-words-joined-by-dashes.*/
 
 function spinalCase(str) {
     str = str.toLowerCase();
@@ -51,16 +49,13 @@ function spinalCase(str) {
 //should return "this-is-spinal-tap".
 console.log(spinalCase('This Is Spinal Tap'));
 
-// Given a positive integer num, return the sum of all odd Fibonacci numbers that are less 
-// than or equal to num.
-
-// The first two numbers in the Fibonacci sequence are 1 and 1. Every additional number in 
-// the sequence is the sum of the two previous numbers. The first six numbers of the 
-// Fibonacci sequence are 1, 1, 2, 3, 5 and 8.
-
-// For example, sumFibs(10) should return 10 because all odd Fibonacci numbers less than 
-// or equal to 10 are 1, 1, 3, and 5.
-
+/* Given a positive integer num, return the sum of all odd Fibonacci numbers that are less 
+than or equal to num.
+The first two numbers in the Fibonacci sequence are 1 and 1. Every additional number in 
+the sequence is the sum of the two previous numbers. The first six numbers of the 
+Fibonacci sequence are 1, 1, 2, 3, 5 and 8.
+For example, sumFibs(10) should return 10 because all odd Fibonacci numbers less than 
+or equal to 10 are 1, 1, 3, and 5. */
 function sumFibs(num) {
 
     var fib = [1, 1]
@@ -78,9 +73,8 @@ function sumFibs(num) {
 console.log(sumFibs(4)); // should return 5
 console.log(sumFibs(4000000)); // should return 4613732.
 
-// Return an English translated sentence of the passed binary string.
-// The binary string will be space separated.
-
+/* Return an English translated sentence of the passed binary string.
+The binary string will be space separated. */
 function binaryAgent(str) {
     return str.split(/\s/).map(function (val) {
         return String.fromCharCode(parseInt(val, 2));
@@ -90,14 +84,13 @@ function binaryAgent(str) {
 binaryAgent("01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111")
 
 
-// Create a function that sums two arguments together. If only one argument 
-// is provided, then return a function that expects one argument and returns the sum.
-// For example, addTogether(2, 3) should return 5, and addTogether(2) should return a function.
-// Calling this returned function with a single argument will then return the sum:
-// var sumTwoAnd = addTogether(2);
-// sumTwoAnd(3) returns 5.
-// If either argument isn't a valid number, return undefined.
-
+/* Create a function that sums two arguments together. If only one argument 
+is provided, then return a function that expects one argument and returns the sum.
+For example, addTogether(2, 3) should return 5, and addTogether(2) should return a function.
+Calling this returned function with a single argument will then return the sum:
+var sumTwoAnd = addTogether(2);
+sumTwoAnd(3) returns 5.
+If either argument isn't a valid number, return undefined. */
 function addTogether(first, second) {
     if (typeof first !== "number") {
         return undefined;
@@ -110,12 +103,11 @@ function addTogether(first, second) {
 addTogether(2, 3) // should return 5.
 addTogether(23, 30) // should return 53.
 
-// Return a new array that transforms the elements' average altitude into their orbital periods 
-// (in seconds). The array will contain objects in the format {name: 'name', avgAlt: avgAlt}.
-// You can read about orbital periods on Wikipedia.
-// The values should be rounded to the nearest whole number. The body being orbited is Earth.
-// The radius of the earth is 6367.4447 kilometers, and the GM value of earth is 398600.4418 km3s-2.
-
+/* Return a new array that transforms the elements' average altitude into their orbital periods 
+(in seconds). The array will contain objects in the format {name: 'name', avgAlt: avgAlt}.
+You can read about orbital periods on Wikipedia.
+The values should be rounded to the nearest whole number. The body being orbited is Earth.
+The radius of the earth is 6367.4447 kilometers, and the GM value of earth is 398600.4418 km3s-2. */
 function orbitalPeriod(arr) {
     var GM = 398600.4418;
     var earthRadius = 6367.4447;
@@ -134,20 +126,19 @@ function orbitalPeriod(arr) {
       master.push({name: arr[i].name, orbitalPeriod: t});
     }
     return master
-  }
+}
 // should return [{name: "sputnik", orbitalPeriod: 86400}]
 orbitalPeriod([{name : "sputnik", avgAlt : 35873.5553}]);
 
 
-// Return true if the given string is a palindrome. Otherwise, return false.
-// A palindrome is a word or sentence that's spelled the same way both forward and backward, ignoring 
-// punctuation, case, and spacing.
-// Note
-// You'll need to remove all non-alphanumeric characters (punctuation, spaces and symbols) and turn 
-// everything into the same case (lower or upper case) in order to check for palindromes.
-// We'll pass strings with varying formats, such as "racecar", "RaceCar", and "race CAR" among others.
-// We'll also pass strings with special symbols, such as "2A3*3a2", "2A3 3a2", and "2_A3*3#A2".
-
+/* Return true if the given string is a palindrome. Otherwise, return false.
+A palindrome is a word or sentence that's spelled the same way both forward and backward, ignoring 
+punctuation, case, and spacing.
+Note
+You'll need to remove all non-alphanumeric characters (punctuation, spaces and symbols) and turn 
+everything into the same case (lower or upper case) in order to check for palindromes.
+We'll pass strings with varying formats, such as "racecar", "RaceCar", and "race CAR" among others.
+We'll also pass strings with special symbols, such as "2A3*3a2", "2A3 3a2", and "2_A3*3#A2". */
 function palindrome(str) {
     let reg = /[\W_\s]/g;
     let matches = str.match(reg);
@@ -158,11 +149,74 @@ function palindrome(str) {
     
     return str.split('').reverse().join('')===str;
   
-  }
-  
-  
+}
+
 // should return true
 palindrome("eye"); 
 // should return true
 palindrome("A man, a plan, a canal. Panama") 
-  
+
+/* Convert the given number into a roman numeral. */
+function convertToRoman(num) {
+    var roman = {
+        M: 1000,
+        CM: 900,
+        D: 500,
+        CD: 400,
+        C: 100,
+        XC: 90,
+        L: 50,
+        XL: 40,
+        X: 10,
+        IX: 9,
+        V: 5,
+        IV: 4,
+        I: 1
+    };
+    var str = '';
+// i will be a key in roman
+    for (var i of Object.keys(roman)) {
+    // check how many times current roman fits into num
+    // if none, q=0
+        var q = Math.floor(num / roman[i]);
+    // subtract our multiplier (q) times our current val
+        num -= q * roman[i];
+    // append roman num to str q times
+        str += i.repeat(q);
+    }
+
+    return str;
+}
+
+
+/* A common modern use is the ROT13 cipher, where the values of the letters are shifted by 13 places. 
+Thus 'A' ↔ 'N', 'B' ↔ 'O' and so on.
+Write a function which takes a ROT13 encoded string as input and returns a decoded string.
+All letters will be uppercase. Do not transform any non-alphabetic character (i.e. spaces, punctuation), 
+but do pass them on. */
+function rot13(str) {
+    return str.replace(/[A-Z]/g, L =>
+      String.fromCharCode((L.charCodeAt(0) % 26) + 65)
+    );
+}
+  // should decode to FREE PIZZA!
+  rot13("SERR CVMMN!") 
+
+
+/*Return true if the passed string looks like a valid US phone number.
+The user may fill out the form field any way they choose as long as it has the format of a valid US 
+number. The following are examples of valid formats for US numbers (refer to the tests below for 
+other variants): 
+555-555-5555
+(555)555-5555
+(555) 555-5555
+555 555 5555
+5555555555
+1 555 555 5555
+*/
+function telephoneCheck(str) {
+  let r = /^(1\s?)?(\d{3}\s?|\(\d{3}\)\s?|\d{3}-)(\d{3})(\s|-)?(\d{4})$/g
+  return r.test(str);
+}
+//
+telephoneCheck("555-555-5555");
